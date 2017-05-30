af6c62bfa0dec31f388b571c4c9bdc91fa6ec092
David Gilardi <david.gilardi@datastax.com>
2017-05-30 15:51:30 -0400

    Updated SuggestedVideoService.getRelatedVideos to use async database calls...mostly The vast majority of calls previously using either session.execute() or *mapper.map() are now using session.executeAsync() and *mapper.mapAsync respectively to prevent blocking operations in an app that should be async.  One caveat is the very last call to videos.get() at line 137.  As I understand it this blocks until return for every call made at this level, but I simply could not find a good way to make this 100% async...yet...for now I will wrap this up, stick a TODO, and move on

--- src/main/java/killrvideo/service/SuggestedVideosService.java
+++ src/main/java/killrvideo/service/SuggestedVideosService.java
@@ -6,13 +6,13 @@ import java.util.*;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-import com.datastax.driver.core.ResultSetFuture;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.querybuilder.BuiltStatement;
+import com.datastax.driver.core.*;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.mapping.MappingManager;
 import com.datastax.driver.mapping.Result;
+import com.google.common.util.concurrent.ListenableFuture;
 import killrvideo.entity.*;
+import killrvideo.utils.FutureUtils;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.map.HashedMap;
 import org.slf4j.Logger;
@@ -48,12 +48,20 @@ public class SuggestedVideosService extends AbstractSuggestedVideoService {
 
     Session session;
     private String videoByTagTableName;
+    private PreparedStatement getRelatedVideos_getVideosByTagPrepared;
 
     @PostConstruct
     public void init(){
         this.session = manager.getSession();
 
         videoByTagTableName = videoByTagMapper.getTableMetadata().getName();
+
+        getRelatedVideos_getVideosByTagPrepared = session.prepare(
+                QueryBuilder
+                        .select().all()
+                        .from(Schema.KEYSPACE, videoByTagTableName)
+                        .where(QueryBuilder.eq("tag", QueryBuilder.bindMarker()))
+        );
     }
 
     @Override
@@ -73,83 +81,89 @@ public class SuggestedVideosService extends AbstractSuggestedVideoService {
         /**
          * Load the source video
          */
-        //:TODO Review to determine if synchronous get() is ok here
-        final Video video = videoMapper.get(videoId);
+        FutureUtils.buildCompletableFuture(videoMapper.getAsync(videoId))
+                .handleAsync((video, ex) -> {
+                    LOGGER.debug("Handler thread " + Thread.currentThread().toString());
+                    if (video == null) {
+                        returnNoResult(responseObserver, builder);
+                        return null;
 
-        if (video == null) {
-            returnNoResult(responseObserver, builder);
+                    } else {
+                        /**
+                         * Return immediately if the source video has
+                         * no tags
+                         */
+                        if (CollectionUtils.isEmpty(video.getTags())) {
+                            returnNoResult(responseObserver, builder);
 
-        } else {
-            /**
-             * Return immediately if the source video has
-             * no tags
-             */
-            if (CollectionUtils.isEmpty(video.getTags())) {
-                returnNoResult(responseObserver, builder);
-
-            } else {
-                final List<String> tags = new ArrayList<>(video.getTags());
-                Map<Uuid, SuggestedVideoPreview> results = new HashedMap();
-
-                /**
-                 * Use the number of results we ultimately want * 2 when querying so that we can account
-                 * for potentially having to filter out the video Id we're using as the basis for the query
-                 * as well as duplicates
-                 **/
-                final int pageSize = RELATED_VIDEOS_TO_RETURN * 2;
-                final List<ResultSetFuture> inFlightQueries = new ArrayList<>();
-
-                /** Kick off a query for each tag and track them in the inflight requests list **/
-                for (int i = 0; i < tags.size(); i++) {
-                    String tag = tags.get(i);
-
-                    ResultSetFuture listAsync;
-
-                    //:TODO Use QueryBuilder in prepared statement with bindmarker()
-                    BuiltStatement statement = QueryBuilder
-                            .select().all()
-                            .from(Schema.KEYSPACE, videoByTagTableName)
-                            .where(QueryBuilder.eq("tag", tag));
-
-                    statement
-                            .setFetchSize(pageSize);
-
-                    inFlightQueries.add(session.executeAsync(statement));
-
-                    /** Every third query, or if this is the last tag, wait on all the query results **/
-                    if (inFlightQueries.size() == 3 || i == tags.size() - 1) {
-                        for (ResultSetFuture future : inFlightQueries) {
-                            try {
-                                //TODO: Potentially move away from getUninterruptibly() and move to async results
-                                Result<VideoByTag> videos = videoByTagMapper.map(future.getUninterruptibly());
-                                results.putAll(videos.all()
-                                        .stream()
-                                        .map(VideoByTag::toSuggestedVideoPreview)
-                                        .filter(previewVid ->
-                                                !previewVid.getVideoId().equals(request.getVideoId())
-                                                && !results.containsKey(previewVid.getVideoId())
-                                        )
-                                        .collect(toMap(preview -> preview.getVideoId(), preview -> preview)));
-
-                            } catch (Exception e) {
-                                responseObserver.onError(Status.INTERNAL.withCause(e).asRuntimeException());
-                            }
-                        }
-
-                        if (results.size() >= RELATED_VIDEOS_TO_RETURN) {
-                            break;
                         } else {
-                            inFlightQueries.clear();
+                            final List<String> tags = new ArrayList<>(video.getTags());
+                            Map<Uuid, SuggestedVideoPreview> results = new HashedMap();
+
+                            /**
+                             * Use the number of results we ultimately want * 2 when querying so that we can account
+                             * for potentially having to filter out the video Id we're using as the basis for the query
+                             * as well as duplicates
+                             **/
+                            final int pageSize = RELATED_VIDEOS_TO_RETURN * 2;
+                            final List<ListenableFuture<Result<VideoByTag>>> inFlightQueries = new ArrayList<>();
+
+                            /** Kick off a query for each tag and track them in the inflight requests list **/
+                            for (int i = 0; i < tags.size(); i++) {
+                                String tag = tags.get(i);
+
+                                BoundStatement statement = getRelatedVideos_getVideosByTagPrepared.bind()
+                                        .setString("tag", tag);
+
+                                statement
+                                        .setFetchSize(pageSize);
+
+                                /**
+                                 * By wrapping my session.executeAsync() method with
+                                 * the DSE mapper videoByTagMapper.mapAsync() function I am
+                                 * 1) automagically mapping the results to VideoByTag entities and
+                                 * 2) automagically creating a prepared statement the mapper will use for
+                                 * any subsequent calls to this same query.
+                                 */
+                                inFlightQueries.add(videoByTagMapper.mapAsync(session.executeAsync(statement)));
+
+                                /** Every third query, or if this is the last tag, wait on all the query results **/
+                                if (inFlightQueries.size() == 3 || i == tags.size() - 1) {
+                                    for (ListenableFuture<Result<VideoByTag>> videos : inFlightQueries) {
+                                        try {
+                                            LOGGER.debug("Handler thread " + Thread.currentThread().toString());
+
+                                            //TODO: Potentially move away from get() and move to async results
+                                            results.putAll(videos.get().all()
+                                                    .stream()
+                                                    .map(VideoByTag::toSuggestedVideoPreview)
+                                                    .filter(previewVid ->
+                                                            !previewVid.getVideoId().equals(request.getVideoId())
+                                                                    && !results.containsKey(previewVid.getVideoId())
+                                                    )
+                                                    .collect(toMap(preview -> preview.getVideoId(), preview -> preview)));
+
+                                        } catch (Exception e) {
+                                            responseObserver.onError(Status.INTERNAL.withCause(e).asRuntimeException());
+                                        }
+                                    }
+
+                                    if (results.size() >= RELATED_VIDEOS_TO_RETURN) {
+                                        break;
+                                    } else {
+                                        inFlightQueries.clear();
+                                    }
+                                }
+                            }
+                            builder.addAllVideos(results.values());
+                            responseObserver.onNext(builder.build());
+                            responseObserver.onCompleted();
+
+                            LOGGER.debug(String.format("End getting related videos with %s videos", results.size()));
                         }
                     }
-                }
-                builder.addAllVideos(results.values());
-                responseObserver.onNext(builder.build());
-                responseObserver.onCompleted();
-
-                LOGGER.debug(String.format("End getting related videos with %s videos", results.size()));
-            }
-        }
+                    return video;
+                });
     }
 
     private void returnNoResult(StreamObserver<GetRelatedVideosResponse> responseObserver, GetRelatedVideosResponse.Builder builder) {
