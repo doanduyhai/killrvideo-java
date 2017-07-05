50445998da565f05268e632697bef231a1ad9574
David Gilardi <david.gilardi@datastax.com>
2017-07-05 14:25:56 -0400

    Added new yaml and helper scripts to allow for different launch configurations from the default, local KillrVideo app Added docker-compose-server.yaml which launches the "server" portion of the app without an IDE.  This is really here so I can launch this headless up on the cloud. Added some helper scripts as well so we don't have to keep typing out long commands.

---
+++ _with_server_and_external_up.sh
@@ -0,0 +1,1 @@
+docker-compose -f lib/killrvideo-docker-common/docker-compose.yaml -f lib/killrvideo-docker-common/docker-compose-dse-external.yaml -f docker-compose.yaml -f docker-compose-server.yaml up -d
---
+++ _with_server_down.sh
@@ -0,0 +1,1 @@
+docker-compose -f lib/killrvideo-docker-common/docker-compose.yaml -f docker-compose.yaml -f docker-compose-server.yaml down
